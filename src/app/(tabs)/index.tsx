0836def31b1fd17995de9bc5ab02bce9af752e22
masch <the.masch@gmail.com>
2026-04-04 20:11:56 -0300

    style: add top padding to the list header view in the index tab

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -21,7 +21,7 @@ export default function ListScreen() {
             contentContainerStyle={{ padding: 20, gap: 14 }}
             contentInsetAdjustmentBehavior='automatic'
             ListHeaderComponent={
-                <View style={{ gap: 14 }}>
+                <View style={{ gap: 14, paddingTop: 20 }}>
                     <TabsScreenBackground />
                     <ListHeroCard />
                     <View className="flex-row items-center justify-between px-1">
