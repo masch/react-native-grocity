c28239f445fe3b140ffdd4a6e482094ae6682d20
masch <the.masch@gmail.com>
2026-04-04 20:04:49 -0300

    feat: Add insights tab

--- src/app/(tabs)/insights.tsx
+++ src/app/(tabs)/insights.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import ClearCompletedButton from '@/components/insights/ClearCompletedButton'
+import InsightsCategorySection from '@/components/insights/InsightsCategorySection'
+import InsightsPrioritySection from '@/components/insights/InsightsPrioritySection'
+import InsightsStatsSection from '@/components/insights/InsightsStatsSection'
+import UserProfile from '@/components/insights/UserProfile'
+import TabScreenBackground from '@/components/TabsScreenBackground'
+import { ScrollView } from 'react-native'
 
 const InsightsScreen = () => {
     return (
-        <View>
-            <Text>insights</Text>
-        </View>
+        <ScrollView
+            className='flex-1 bg-background py-4'
+            contentContainerStyle={{ padding: 20, gap: 14 }}
+            showsVerticalScrollIndicator={false}
+            contentInsetAdjustmentBehavior='automatic'
+        >
+            <TabScreenBackground />
+            <UserProfile />
+            <InsightsStatsSection />
+            <InsightsCategorySection />
+            <InsightsPrioritySection />
+            <ClearCompletedButton />
+        </ScrollView>
     )
 }
 
