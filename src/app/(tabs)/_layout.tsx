b9b7d1df2059fa1d1562a44efc027e890e7412a3
masch <the.masch@gmail.com>
2026-04-04 11:06:38 -0300

    chore: Update tab styles

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,9 +1,15 @@
-import { useAuth } from '@clerk/expo'
-import { Redirect } from 'expo-router'
-import { NativeTabs } from 'expo-router/unstable-native-tabs'
+import { useAuth } from '@clerk/expo';
+import { Redirect } from 'expo-router';
+import { NativeTabs } from 'expo-router/unstable-native-tabs';
+import { useColorScheme } from "nativewind";
 
 export default function TabsLayout() {
     const { isSignedIn, isLoaded } = useAuth()
+
+    const { colorScheme } = useColorScheme()
+    const isDark = colorScheme === "dark";
+    const tabTintColor = isDark ? "hsl(142 70% 54%)" : "hsl(147 75% 33%)";
+
 
     if (!isLoaded) {
         return null
@@ -13,20 +19,26 @@ export default function TabsLayout() {
         return <Redirect href="/(auth)/sign-in" />
     }
 
-    return <NativeTabs >
+    return <NativeTabs tintColor={tabTintColor}>
         <NativeTabs.Trigger name="index">
             <NativeTabs.Trigger.Label>List</NativeTabs.Trigger.Label>
-            <NativeTabs.Trigger.Icon sf="list.bullet.clipboard" md="list" />
+            <NativeTabs.Trigger.Icon
+                sf={{ default: "list.bullet.clipboard", selected: "list.bullet.clipboard.fill" }}
+                md="list" />
         </NativeTabs.Trigger>
 
         <NativeTabs.Trigger name="planner">
             <NativeTabs.Trigger.Label>Planner</NativeTabs.Trigger.Label>
-            <NativeTabs.Trigger.Icon sf="plus.circle" md="add" />
+            <NativeTabs.Trigger.Icon
+                sf={{ default: "plus.circle", selected: "plus.circle.fill" }}
+                md="add" />
         </NativeTabs.Trigger>
 
         <NativeTabs.Trigger name="insights">
             <NativeTabs.Trigger.Label>Insights</NativeTabs.Trigger.Label>
-            <NativeTabs.Trigger.Icon sf="chart.bar" md="analytics" />
+            <NativeTabs.Trigger.Icon
+                sf={{ default: "chart.bar", selected: "chart.bar.fill" }}
+                md="analytics" />
         </NativeTabs.Trigger>
 
     </NativeTabs>
